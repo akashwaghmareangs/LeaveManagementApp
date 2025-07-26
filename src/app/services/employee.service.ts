98fec75a858407a585d2f84628825f2e67679e8f
Akash Waghmare <akashwaghmareangs@gmail.com>
2025-07-26 21:19:51 +0530

    employeeDetails_geted

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { APIResponseModel } from '../modal/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  onLogin(obj:any){
-    return this.http.post("https://freeapi.miniprojectideas.com/api/EmployeeLeave/Login",obj)
+  onLogin(obj: any) {
+    return this.http.post("https://freeapi.miniprojectideas.com/api/EmployeeLeave/Login", obj)
+  }
+
+  getAllEmployess(): Observable<APIResponseModel> {
+    return this.http.get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/EmployeeLeave/GetEmployees")
   }
 }
